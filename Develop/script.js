05d2b3e5c342d4022a7e21fcfab3a5a13e45e2db
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-28 01:14:23 +1000

    add more code

--- Develop/script.js
+++ Develop/script.js
@@ -1,78 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
-
-//the code will run when DOM is fully loaded
 $(document).ready(function() {
   const currentDay = dayjs().format('dddd, MMMM D, YYYY');
-  //display current Day to the top of Calender
   $('#currentDay').text(currentDay);
 
-  const container = $('.container-fluid');
-  const currentHour = dayjs().hour();
+  $('.saveBtn').on('click', function() {
+    const textInput = $(this).siblings(".description").val();
+    const parentId = $(this).parent().attr('id');
+    localStorage.setItem(parentId, textInput);
+  });
 
- // loop through hours 10am to 5pm.
- for (let hour = 10; hour <= 17; hour++) {
-  const timeBlock = $('<div>').attr('id', 'hour-' + hour).addClass('row time-block');
-  container.append(timeBlock);
-  console.log(container);
- 
-  const hourEl = $('<div>').addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour, 'H')).format('hA');
-  timeBlock.append(hourEl);
+  function populateTextareas() {
+    const currentTime = dayjs().hour();
+    const startHour = 10;
+    const endHour = 17;
+    const hourLabels = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
-  const textarea = $('<textarea>').addClass("col-8 col-md-10 description").attr("rows", "3");
-  timeBlock.append(textarea);
+    for (let hour = startHour; hour <= endHour; hour++) {
+      const timeBlock = $('<div>').attr('id', `hour-${hour}`).addClass('row time-block');
+      const hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(hourLabels[hour - startHour]);
+      const textarea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+      const saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+      const saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
 
-  const saveButton = $('<button>').addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
-  const saveIcon = $('<li>').addClass("fas fa-save").attr("aria-hidden", "true");
-  saveBtn.append(saveIcon);
-  timeBlock.append(saveButton);
+      timeBlock.attr('id', `hour-${hour}`);
+      saveBtn.append(saveIcon);
+      timeBlock.append(hourDiv, textarea, saveBtn);
+      $('.container-lg').append(timeBlock);
 
-  //add class to the hour
-  if (hour < currentHour) {
-    timeBlock.addClass('past');
-  } else if (hour === currentHour) {
-    timeBlock.addClass('present');
-  } else if (hour > currentHour) {
-    timeBlock.addClass('future')
+      //apply past, present, future style based on current hour
+      if (hour < currentTime) {
+        timeBlock.addClass('past');
+      } else if (hour === currentTime) {
+        timeBlock.addClass('present');
+      } else {
+        timeBlock.addClass('future');
+      }
+    }
+
+    $('.time-block').each(function() {
+      const timeBlockId = $(this).attr('id');
+      const savedValue = localStorage.getItem(timeBlockId);
+      $(this).find('.description').val(savedValue);
+    });
   }
- }
 
- //Load saved event from local storage
- for (let i = 10; i <= 17; i++) {
-  const savedEvent = localStorage.getItem('hour-' + i);
-  $('#hour-' + i + " .description").val(savedEvent);
-  
- }
-
- //add event listener to saved button
- $('.saveBtn').on('click', function() {
-  const userInput = $(this).siblings('.description').val()
-  const timeBlockId = $(this).parent().attr('id')
-
-  //save to localStorage
-  localStorage.setItem(timeBlockId, userInput);
- });
+  populateTextareas();
 });
-
-
