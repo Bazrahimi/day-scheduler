694bbd9753a1f552f8f549a6ff29f67be4940ca3
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-28 02:40:04 +1000

    final check

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,24 @@
+  //the code will run after the DOM load
 $(document).ready(function() {
+  //get the Current using Day.js
   const currentDay = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(currentDay);
 
- 
+  //Event listener for the save button
   $(document).on('click', '.saveBtn', function() {
     const textInput = $(this).siblings(".description").val();
     const parentId = $(this).parent().attr('id');
     localStorage.setItem(parentId, textInput);
   });
 
+  //function to populate textarea
   function populateTextareas() {
     const currentTime = dayjs().hour();
-    const startHour = 10;
+    const startHour = 9;
     const endHour = 17;
-    const hourLabels = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+    const hourLabels = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
+    //loop through each hour
     for (let hour = startHour; hour <= endHour; hour++) {
       const timeBlock = $('<div>').attr('id', `hour-${hour}`).addClass('row time-block');
       const hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(hourLabels[hour - startHour]);
@@ -22,12 +26,13 @@ $(document).ready(function() {
       const saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
       const saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
 
+      //set the time Block ID and append hour, textarea, and save button
       timeBlock.attr('id', `hour-${hour}`);
       saveBtn.append(saveIcon);
       timeBlock.append(hourDiv, textarea, saveBtn);
       $('.container-lg').append(timeBlock);
 
-      //apply past, present, future style based on current hour
+      //apply past, present and future style base on current time
       if (hour < currentTime) {
         timeBlock.addClass('past');
       } else if (hour === currentTime) {
@@ -36,7 +41,7 @@ $(document).ready(function() {
         timeBlock.addClass('future');
       }
     }
-
+    //get the saved value from local storage 
     $('.time-block').each(function() {
       const timeBlockId = $(this).attr('id');
       const savedValue = localStorage.getItem(timeBlockId);
