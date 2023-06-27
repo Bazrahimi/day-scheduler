b5e58bf5d8eb73f1aca4b0f5a39785a83194e393
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-27 18:15:28 +1000

    add code

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,59 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+// });
+
+//the code will run when DOM is fully loaded
+$(document).ready(function() {
+  const currentDay = dayjs().format('dddd, MMMM D, YYYY');
+  //display current Day to the top of Calender
+  $('#currentDay').text(currentDay);
+
+  const container = $('.container-fluid');
+  const currentHour = dayjs().hour();
+
+ // loop through hours 10am to 5pm.
+ for (let hour = 10; hour <= 17; hour++) {
+  const timeBlock = $('<div>').attr('id', 'hour-' + hour).addClass('row time-block');
+  container.append(timeBlock);
+  console.log(container);
+ 
+  const hourEl = $('<div>').addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour, 'H')).format('hA');
+  timeBlock.append(hourEl);
+
+  const textarea = $('<textarea>').addClass("col-8 col-md-10 description").attr("rows", "3");
+  timeBlock.append(textarea);
+
+  const saveButton = $('<button>').addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+  const saveIcon = $('<li>').addClass("fas fa-save").attr("aria-hidden", "true");
+  saveBtn.append(saveIcon);
+  timeBlock.append(saveButton);
+
+  //add class to the hour
+  if (hour < currentHour) {
+    timeBlock.addClass('past');
+  } else if (hour === currentHour) {
+    timeBlock.addClass('present');
+  } else if (hour > currentHour) {
+    timeBlock.addClass('future')
+  }
+ }
+
+ //Load saved event from local storage
+ for (let i = 10; i <= 17; i++) {
+  const savedEvent = localStorage.getItem('hour-' + i);
+  $('#hour-' + i + " .description").val(savedEvent);
+  
+ }
+
+ //add event listener to saved button
+ $('.saveBtn').on('click', function() {
+  const userInput = $(this).siblings('.description').val()
+  const timeBlockId = $(this).parent().attr('id')
+
+  //save to localStorage
+  localStorage.setItem(timeBlockId, userInput);
+ });
 });
+
+
